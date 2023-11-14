5197e6c7461c37d13564e88e108b3fdb29934fb8
Chandrakant Shinde <chandrakantshinde7348@gmail.com>
2023-11-14 21:59:58 +0530

    unit tests added

---
+++ test/jobs.test.js
@@ -0,0 +1,6 @@
+const request = require("supertest");
+const BASE_URL = "http://localhost:7000/api/v1/jobs";
+
+describe("Get Job By Id", () => {
+  it("should return a list of jobs", (done) => {});
+});
