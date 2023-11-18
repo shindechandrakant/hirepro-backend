cb5be7e6777a3cfdd6c9f7cb202935889acdf113
Chandrakant Shinde <chandrakantshinde7348@gmail.com>
2023-11-18 14:33:53 +0530

    GET /job/:jobId response changed

--- entity/JobField.js
+++ entity/JobField.js
@@ -2,16 +2,22 @@ import { sequelize } from "../utils/databaseConnection.js";
 import { Job } from "./jobs.js";
 import { DataTypes } from "sequelize";
 
-export const JobField = sequelize.define("job_field", {
-  field_name: {
-    type: DataTypes.STRING(50),
+export const JobField = sequelize.define(
+  "job_field",
+  {
+    field_name: {
+      type: DataTypes.STRING(50),
+    },
+    field_description: {
+      type: DataTypes.STRING(1024),
+    },
+    job_id: {
+      type: DataTypes.STRING(10),
+    },
   },
-  field_description: {
-    type: DataTypes.STRING(1024),
-  },
-  job_id: {
-    type: DataTypes.STRING(10),
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 JobField.belongsTo(Job, { foreignKey: "job_id" });
